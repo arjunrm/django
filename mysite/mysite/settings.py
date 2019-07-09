a629083c027c5e377c819ad039c6a74e2fba4f25
Arjun Ramamurthy (arj3kor) <arjun.r@in.bosch.com>
2019-07-09 11:58:49 +0530

    added support for docker

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9dnu54ad=o&7_4oqhw2id%b9pofm)p495-^iz*lz&sid-n!jzi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
